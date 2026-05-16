b5fd7c3b2a2a1715eca370afee00f3ab77f3ac24
Kristian Gjidodaj <kristiangjidodaj@gmail.com>
2026-05-16 21:51:04 +0300

    fix: hardened USB backup script and added Dozzle observability Details: - Changed tar to rsync for delta backups to reduce USB I/O wear and tear - Fixed cron pathing adding '$HOME' - Fixed syntax errors and added sudo to mounts - Added Dozzle container for L7 log trailing and updated .gitignore

--- backup.sh
+++ backup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-source .env 
+source "$HOME/.env"
 
 Backup_dest="/mnt/usb_backup"
 Source_dir="$location"
 Date=$(date +%F)
 
-echo -e "Mounting USB drive.../n"
-mount $Backup_dest
+echo -e "Mounting USB drive...\n"
+sudo mount $Backup_dest #using uuid on fstab
 
 if mountpoint -q $Backup_dest;then #checking mounting in the background
 	echo "Mount succesful. Starting archive..."
 
 	#keeping permissions and compressing the archive
-	tar -czpPf "$Backup_dest/homelab_backup_$DATE.tar.gz" "$Source_dir"
+	sudo rsync -av --delete  "$Source_dir" "$Backup_dest/homelab_backup"
 	if [ $? -eq 0 ]; then #checking exit code
 		echo "Backup completed succesfully: Date=$Date"
 	else
-		echo "ERROR: tar failed"
+		echo "ERROR: rsync failed"
 	fi
 
 	echo " Unmounting USB" #using umount for security
-	umount $Backup_dest
+	sudo umount $Backup_dest
 	echo "Done unmounting"
 else
 	echo "ERROR: mountpoint failed usb not found or mount failed"
