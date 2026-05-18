f033f0238884769543e152ed5aadf7655156996b
Kristian Gjidodaj <kristiangjidodaj@gmail.com>
2026-05-18 17:59:10 +0300

    fix:resolve Shellcheck syntax warnings and variables in automation scripts

---
+++ scripts/backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+source "$HOME/.env"
+
+Backup_dest="/mnt/usb_backup"
+Source_dir="$location"
+Date=$(date +%F)
+
+echo -e "Mounting USB drive...\n"
+sudo mount "$Backup_dest" #using uuid on fstab
+
+if mountpoint -q "$Backup_dest";then #checking mounting in the background
+	echo "Mount succesful. Starting archive..."
+
+	#keeping permissions and compressing the archive
+	if sudo rsync -av --delete  "$Source_dir" "$Backup_dest/homelab_backup"; then
+		echo "Backup completed succesfully: Date=$Date"
+	else
+		echo "ERROR: rsync failed"
+	fi
+
+	echo " Unmounting USB" #using umount for security
+	sudo umount $Backup_dest
+	echo "Done unmounting"
+else
+	echo "ERROR: mountpoint failed usb not found or mount failed"
+	exit 1 #failure exit code
+fi
+
